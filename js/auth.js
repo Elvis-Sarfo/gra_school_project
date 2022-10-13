6e97964d990402b7e0860befe72988d7ddc52e60
Kanta <sarfoantwielvis@gmail.com>
2022-10-13 07:02:04 +0000

    fixed auth

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,6 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-var user = auth.currentUser;
-console.log(user);
-if (user) {
-  // User is signed in.
-} else {
-  // No user is signed in.
-}
-{/* <await onAuthStateChanged(auth, (_user) => {
+onAuthStateChanged(auth, (_user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
@@ -15,7 +8,16 @@ if (user) {
 
   } else {
     // User is signed out
-    window.location = '/'
+    localStorage.removeItem('authUser')
   }
 });
-console.log(user);> */}
+
+var user = localStorage.getItem('authUser');
+console.log(user);
+if (user) {
+  // User is signed in.
+} else {
+  // No user is signed in.
+  window.location = '/'
+}
+console.log(user)
